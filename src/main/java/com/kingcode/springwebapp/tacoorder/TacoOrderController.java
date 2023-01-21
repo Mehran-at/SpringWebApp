5d732b81c291af539725b9716078f36da7dfca21
Mehran Hosseini <mehran.hosseini@ebcont.com>
2023-01-21 15:47:36 +0100

    Add ordersForUser with pagination

--- src/main/java/com/kingcode/springwebapp/tacoorder/TacoOrderController.java
+++ src/main/java/com/kingcode/springwebapp/tacoorder/TacoOrderController.java
@@ -1,22 +1,34 @@
 package com.kingcode.springwebapp.tacoorder;
 
 import com.kingcode.springwebapp.user.User;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
+import org.springframework.data.domain.Pageable;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
 @RequiredArgsConstructor
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "taco.orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
 public class TacoOrderController {
+
+    private int pageSize = 20;
 
     private final TacoOrderRepository orderRepo;
 
@@ -55,10 +67,21 @@ public class TacoOrderController {
 
         order.setUser(user);
 
+        assert orderRepo != null;
         orderRepo.save(order);
         sessionStatus.setComplete();
 
         return "redirect:/";
+    }
+
+
+//     to list the authenticated user’s past orders
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, pageSize);
+        assert orderRepo != null;
+        model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
     }
 
 }
