945ad7c77b91ccba937f6f7383443cbe2795526c
Mehran-at <ajaxcube@gmail.com>
2023-03-14 00:57:35 +0100

    Introducing Reactor Flux - Mono

---
+++ src/main/java/com/kingcode/springsequrity/FluxExamples.java
@@ -0,0 +1,12 @@
+package com.kingcode.springsequrity;
+
+import lombok.Data;
+
+public class FluxExamples {
+
+    @Data
+    static class Player {
+        private final String firstName;
+        private final String lastName;
+    }
+}
--- src/main/java/com/kingcode/springsequrity/user/Role.java
+++
@@ -1,6 +0,0 @@
-package com.kingcode.springsequrity.user;
-
-public enum Role {
-    USER,
-    ADMIN
-}
